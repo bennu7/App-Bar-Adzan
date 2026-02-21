0e3d3fbcef8f6fe7a4756a6d44fd822f7eaa22a5
Lalu Ibnu Hidayatullah <laluibnuhidayatullah@gmail.com>
2026-02-21 19:36:43 +0700

    Initial Commit

---
+++ AdzanBar/AdzanBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdzanBarApp.swift
+//  AdzanBar
+//
+//  Created by lalu ibnu on 21/02/26.
+//
+
+import SwiftUI
+
+@main
+struct AdzanBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
